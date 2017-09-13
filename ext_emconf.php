8186258b4746e6d0b60afc6885f4e51e63b801c6
Tobias Kummer <tobias.kummer@lstschenker.ch>
2017-09-13 10:09:17 +0200

    [TASK] Make extension compatible to TYPO3 8.7 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,12 +21,12 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '2.1.3',
+  'version' => '2.1.4',
   'constraints' => 
   array (
     'depends' => 
     array (
-      'typo3' => '7.5.0-8.6.99',
+      'typo3' => '7.5.0-8.99.99',
     ),
     'conflicts' => 
     array (
@@ -37,4 +37,3 @@ $EM_CONF[$_EXTKEY] = array (
   ),
   '_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"0fa0";s:13:"composer.json";s:4:"3b86";s:12:"ext_icon.png";s:4:"dc44";s:14:"ext_tables.php";s:4:"5766";s:39:"Classes/Controller/DomainController.php";s:4:"4694";s:31:"Classes/Domain/Model/Domain.php";s:4:"95a4";s:46:"Classes/Domain/Repository/DomainRepository.php";s:4:"5e82";s:34:"Classes/Service/PatternService.php";s:4:"0860";s:49:"Classes/ViewHelpers/Be/Buttons/IconViewHelper.php";s:4:"1862";s:34:"Configuration/TypoScript/setup.txt";s:4:"7901";s:33:"Migrations/Code/ClassAliasMap.php";s:4:"66dd";s:44:"Resources/Private/Language/locallang_mod.xlf";s:4:"1b6e";s:44:"Resources/Private/Language/locallang_mod.xml";s:4:"58bc";s:38:"Resources/Private/Layouts/Default.html";s:4:"7847";s:45:"Resources/Private/Templates/Domain/Index.html";s:4:"1855";s:46:"Resources/Public/Icons/module-hostspattern.svg";s:4:"7c7d";s:41:"Tests/Unit/Service/PatternServiceTest.php";s:4:"7430";}',
 );
-
--- ext_tables.php
+++ ext_tables.php
@@ -2,26 +2,31 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+call_user_func(
+    function ($extKey) {
+        if (TYPO3_MODE === 'BE') {
 
-if (TYPO3_MODE === 'BE') {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'IchHabRecht.hosts_pattern',
-        'tools',
-        'HostsPattern',
-        '',
-        [
-            'Domain' => 'index',
-        ],
-        [
-            'access' => 'admin',
-            'icon' => 'EXT:hosts_pattern/Resources/Public/Icons/module-hostspattern.svg',
-            'labels' => 'LLL:EXT:hosts_pattern/Resources/Private/Language/locallang_mod.xml',
-        ]
-    );
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'IchHabRecht.hosts_pattern',
+                'tools',
+                'HostsPattern',
+                '',
+                [
+                    'Domain' => 'index',
+                ],
+                [
+                    'access' => 'admin',
+                    'icon' => 'EXT:' . $extKey . '/Resources/Public/Icons/module-hostspattern.svg',
+                    'labels' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_mod.xml',
+                ]
+            );
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-        'hosts_pattern',
-        'setup',
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:hosts_pattern/Configuration/TypoScript/setup.txt">'
-    );
-}
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+                'hosts_pattern',
+                'setup',
+                '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/setup.txt">'
+            );
+        }
+    },
+    $_EXTKEY
+);
